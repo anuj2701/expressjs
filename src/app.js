99d9a1152da8d8152a2db1a7fb0f457c5dd95d05
anuj2701 <nagpureanuj2701@gmail.com>
2021-06-28 23:03:09 +0530

    :wq exit modified:   package.json

--- src/app.js
+++ src/app.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const path = require("path");
-
+const hbs = require("hbs")
 const app = express();
 
 port = 8000;
 console.log(path.join(__dirname));
 const staticPath = path.join(__dirname, "../public");
-const templatePath = path.join(__dirname, "../templates");
+const templatePath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
 app.set("view engine", "hbs");
 app.set("views", templatePath);
-
+hbs.registerPartials(partialsPath)
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.use(express.static(staticPath));
 // app.use(express.static(viewPath));
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("*", (req, res) => {
+  res.render("404",{
+    errorcomment:"opps page couldn't found "
+  });
 });
 
 app.listen(port, () => {
